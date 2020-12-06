18887f49beb10d5a27108cbb00d4675834cdd7eb
piotex <pkubon2@gmail.com>
2020-12-06 22:57:29 +0100

    watching webbrowser - 0.5 complete

--- fox/Form1.Designer.cs
+++ fox/Form1.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1870, 0);
+            this.button1.Location = new System.Drawing.Point(1402, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 70);
+            this.button1.Size = new System.Drawing.Size(112, 57);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1335, 559);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2022, 953);
+            this.ClientSize = new System.Drawing.Size(1516, 774);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
@@ -59,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- fox/Form1.cs
+++ fox/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using fox.My_code.browser;
 using fox.My_code.browser.basics;
@@ -25,8 +26,29 @@ namespace fox
             //manager = new Client_Manager(this);
             //manager.NavigatoToMain_Client(Client_Type.ySense);
             //manager.LogIn_Client(Client_Type.ySense);
-            List<loginData_Model> l = MyXmlReader<loginData_Model_List>.Read(@"C:\Users\pkubo\OneDrive\Desktop\YouTubeMaster\fox\XML\loginData.xml").YouTubeProfile_list;
+            List<loginData_Model> l = MyXmlReader<loginData_Model_List>.Read(@"C:\Users\pkubo\OneDrive\Desktop\YouTubeMaster_git\fox\XML\loginData.xml").YouTubeProfile_list;
 
+
+            //GeckoWebBrowser browser = new GeckoWebBrowser();
+            //browser.FrameEventsPropagateToMainWindow = true;
+            //browser.Location = new System.Drawing.Point(0, 0);
+            //browser.Name = "FoX";
+            //browser.Size = new System.Drawing.Size(1920, 1080);
+            //browser.TabIndex = 0;
+            //browser.UseHttpActivityObserver = true;
+            //Controls.Add(browser);
+            //browser.Navigate(@"https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Dpl%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=pl&gae=cb-p23934712&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+            //browser.Navigate(@"https://www.youtube.com/watch?v=se1zpkvDVAs");
+
+            Random random = new Random();
+            int length = random.Next(8,20);
+            const string chars = "qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string hash =  new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            string url = @"https://www.youtube.com/watch?v=HM0R4qJFjyY&list=PL7s3qa2IEcmENu1KvSnYv_t9bKeXUQ5bp&index=1&fbclid=IwAR2sdgdgsdgsdg" + hash;
+            //webBrowser1.Navigate(url);
+            Form_Slave form2 = new Form_Slave(url, "pkubon3@gmail.com", "Start12345");
+            form2.Size = new System.Drawing.Size(1600, 720);
+            form2.ShowDialog();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -34,6 +56,13 @@ namespace fox
             //FoxXmlData db = new FoxXmlData();
             //db.Read();
             //manager.NavigatoToMain_Client(Client_Type.ySense);
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+
         }
     }
 }
