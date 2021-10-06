da4cc6c9171a50e75f979ac9ed97917cda60d1b5
piotex <pkubon2@gmail.com>
2021-10-06 15:02:49 +0200

    Added file witch times for watching

--- fox_YT/FileGenerator/Program.cs
+++ fox_YT/FileGenerator/Program.cs
@@ -6,28 +6,41 @@ namespace FileGenerator
 {
     class Program
     {
-        static int average_watch_time = 7*60;
+        static int average_watch_time = 7*60;                                           //number of seconds of yt video - need to be edited for different videos
         static void Main(string[] args)
         {
             long sum = 0;
-
             int x_seconds = average_watch_time;
-            List<int> y_watchTime = new List<int>();
+            List<int> y_watchTime = getWatchTime(x_seconds);
+            List<int> finalValues = new List<int>();
 
-            for (int i = 0; i < x_seconds; i++)
+            for (int i = 0; i < y_watchTime.Count; i++)
             {
-                int val = getValueOfBaseFunction(i);
-                val = Add_White_Noise_To_Function(val);
-                y_watchTime.Add(val);
-
+                sum += y_watchTime[i];
+                for (int j = 0; j < y_watchTime[i]; j++)
+                {
+                    int time = i;
+                    if (time == 0)
+                    {
+                        time =1;
+                    }
+                    finalValues.Add(time);
+                }
             }
+            Shuffle(ref finalValues);
 
-            using (StreamWriter writer = new StreamWriter(@"..\..\..\..\YT_Master\Files\WatchingTimes.txt"))
+            using (StreamWriter writer = new StreamWriter(@"..\..\..\..\YT_Master\Files\WatchingTimesSumary.txt"))
             {
                 for (int i = 0; i < y_watchTime.Count; i++)
                 {
                     writer.WriteLine(y_watchTime[i]);
-                    sum += y_watchTime[i];
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(@"..\..\..\..\YT_Master\Files\WatchingTimes.txt"))
+            {
+                for (int i = 0; i < finalValues.Count; i++)
+                {
+                    writer.WriteLine(finalValues[i]);
                 }
             }
             Console.WriteLine("Suma Wyswietlen: " + sum);
@@ -55,7 +68,18 @@ namespace FileGenerator
 
 
 
+        static List<int> getWatchTime(int x)
+        {
+            List<int> y_watchTime = new List<int>();
 
+            for (int i = 0; i < x; i++)
+            {
+                int val = getValueOfBaseFunction(i);
+                val = Add_White_Noise_To_Function(val);
+                y_watchTime.Add(val);
+            }
+            return y_watchTime;
+        }
         static int power(int x)
         {
             return x * x;
@@ -70,6 +94,18 @@ namespace FileGenerator
             int move_up = 5;                                               //inportant for small variables
             int shift = average_watch_time;
             return power(x-shift) / correction + move_up;
+        }
+        public static void Shuffle(ref List<int> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = new Random().Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
